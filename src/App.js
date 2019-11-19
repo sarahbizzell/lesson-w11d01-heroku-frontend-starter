803f835cecb98b4a906033b115a73b0e407473c2
Marc Wright <marc.wright@gmail.com>
2019-11-19 07:27:23 -0500

    Add component did mount

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const herokuUrl = 'https://project3-backend-test.herokuapp.com'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    axios({
+      url: `${herokuUrl}/api/users`,
+      method: 'get'
+    })
+      .then(users => {
+        console.log(users)
+        this.setState({ users })
+      })
+  }
+
+  render() {
+    console.log(this.state.users)
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <p>Test</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

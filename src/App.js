306ee750b958beeb2bb99f3c134e1cdf456a9d77
sarahbizzell <sarahbizz01@gmail.com>
2019-12-09 12:13:33 -0500

    update dtabaseUrl

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 // const databaseUrl = process.env.HEROKU_DB_URL || 'http://localhost:3000'
-const databaseUrl = 'https://project3-backend-test.herokuapp.com'
+const databaseUrl = 'https://heroku-deployment-backend-sb.herokuapp.com/'
 
 class App extends React.Component {
   state = {
